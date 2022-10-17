aa3ff644590812292e2c682538fec62ba1419736
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2022-10-18 00:03:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,35 +53,35 @@ function wrongNameAlert() {
     Notiflix.Notify.failure('Oops, there is no country with that name');
 }
   
-function markupCountryInfo(country) {
-    const layoutCountryInfo = country
+function markupCountryList(country) {
+    const layoutCountryList = country
         .map(({name, flags}) => {
             const layout = `
-                <li class="country-list_item">
-                    <img class="country-list__item--flag src="${flags.svg}" alt="Flag of ${name.official}">
+                <li class="country-list__item">
+                    <img class="country-list__item--flag" src="${flags.svg}" alt="Flag of ${name.official}">
                     <h2 class="country-list__item--name">${name.official}</h2>
                 </li>`
             return layout;
         })
         .join(``);
-    return layoutCountryInfo;
+    return layoutCountryList;
 }
 
-function markupCountryList(country) {
-    const layoutCountryList = country
+function markupCountryInfo(country) {
+    const layoutCountryInfo = country
         .map(({ name, flags, capital, population, languages }) => {
             const layout = `
                 <ul class="country-info__list">
-                    <li class="country-info__item>
+                    <li class="country-info__item">
                         <img class="country-info__item--flag" src="${flags.svg}" alt="Flag of ${name.official}">
                         <h2 class="country-info__item--name">${name.official}</h2>
                     </li>
-                    <li class="country-info__item><span class="country-info__item--categories">Capital: </span>${capital}</li>
-                    <li class="country-info__item><span class="country-info__item--categories">Population: </span>${population}</li>
-                    <li class="country-info__item><span class="country-info__item--categories">Languages: ${Object.values(languages,).join(`, `)}</li>
+                    <li class="country-info__item"><span class="country-info__item--categories">Capital: </span>${capital}</li>
+                    <li class="country-info__item"><span class="country-info__item--categories">Population: </span>${population}</li>
+                    <li class="country-info__item"><span class="country-info__item--categories">Languages: </span>${Object.values(languages,).join(`, `)}</li>
                 </ul>`
             return layout;
         })
         .join(``);
-    return layoutCountryList;
+    return layoutCountryInfo;
 }
